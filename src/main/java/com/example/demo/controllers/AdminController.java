2b8fa49858d57ca7740218985f9e5394124a809e
Djabrail <jabrail8@mail.ru>
2021-01-21 01:18:12 +0300

    Добавление шапки

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -3,9 +3,16 @@ package com.example.demo.controllers;
 import com.example.demo.models.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 @RequestMapping("user/admin")
@@ -15,7 +22,19 @@ public class AdminController {
 
     @GetMapping()
     public String index(Model model){
-        model.addAttribute("users",userService.index());
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) auth.getPrincipal();//get logged in username
+        List<GrantedAuthority> list = (List<GrantedAuthority>) auth.getAuthorities();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(GrantedAuthority el : list) {
+            stringBuilder.append(el.getAuthority().toString()).append(" ");
+        }
+
+        model.addAttribute("authorityString", stringBuilder.toString());
+        model.addAttribute("currentUser", currentUser);
+        model.addAttribute("users", userService.index());
         return "user/admin/index";
     }
 
